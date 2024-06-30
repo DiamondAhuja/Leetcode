767926056e4a414a218aced32863b2eca405209f
DiamondAhuja <ahujadiamond@gmail.com>
2024-06-30 16:39:54 -0400

    Refactor code for heightChecker, maxCount, and searchBST

--- Easy/1051. Height Checker.py
+++ Easy/1051. Height Checker.py
@@ -1,8 +1,11 @@
+from typing import List
+
+
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         numInd = 0
         expected = sorted(heights)
-        for i in range (len(heights)):
-            if heights[i] != expected[i]: 
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
                 numInd += 1
         return numInd
--- Easy/598. Range Addition II.py
+++ Easy/598. Range Addition II.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
         for a, b in ops:
